fe70928f393ea316e8f8663aed94e928db0c8987
BSP-SP <57468978+BSP-SP@users.noreply.github.com>
2023-07-18 19:03:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ similarity = pickle.load(open('similarity.pkl','rb'))
 
 # Fetch posters from the TMDb database
 def fetch_poster(movie_id):
-    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=Putyourkey&language=en-US'.format(movie_id))
+    response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US')
     data = response.json()
     
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
